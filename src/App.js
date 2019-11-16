8af9e40b83858da0ca728edcafbdaf02543a621f
kkudryashov <kkudryashov>
2019-11-16 17:01:30 +0300

    Add getProductsByStore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getRecipe, getRecipeList} from "./requestFunctions";
+import {getRecipe, getRecipeList, getProductsByStore} from "./requestFunctions";
 
 class App extends Component {
     constructor(props) {
--- src/requestFunctions.js
+++ src/requestFunctions.js
@@ -33,16 +33,37 @@ export const getRecipe = async (id) => {
     return response.results;
 };
 
-export const getStoresNearby = async (id) => {
-    const objectToSend = {"filters": {
-            "locationDistance" : {
+export const getStoresNearby = async (id) =>
+{
+    const objectToSend = {
+        "filters": {
+            "locationDistance": {
                 "location": STUB_LOCATION,
                 "distance": 1
             }
         }
     };
 
-    const rawResponse = await fetch(`${REQUEST_URL}/${API_VER}/search/stores`, {
+    const rawResponse = await
+    fetch(`${REQUEST_URL}/${API_VER}/search/stores`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': JSON_TYPE,
+            'Ocp-Apim-Subscription-Key': APP_KEY,
+        },
+        body: JSON.stringify(objectToSend),
+    });
+    return await  rawResponse.json();
+};
+
+export const getProductsByStore = async (storeId) =>
+{
+    const objectToSend = {"filters": {
+            "storeAvailability" : storeId.toString(),
+            }
+        };
+
+    const rawResponse = await fetch(`${REQUEST_URL}/${API_VER}/search/products`, {
         method: 'POST',
         headers: {
             'Content-Type': JSON_TYPE,

e2baf27d5432c36d2f3395bc6877f1b72b4e2a37
Igor Siluianov <olvin.hh@gmail.com>
2019-11-16 18:04:49 +0300

    add picture link constant

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,4 @@ export const API_VER = 'v1';
 export const JSON_TYPE = 'application/json;charset=utf-8';
 export const STUB_LOCATION = { "lon": 24.8751406, "lat": 60.1603071 };
 export const STORES_SEARCH_DISTANCE = 1;
+export const PICTURE_LINK = 'https://k-file-storage-qa.imgix.net/f/k-ruoka/recipe/';

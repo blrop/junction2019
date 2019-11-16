44e14b7a5d1cbb96d57dd5f02ecdc6c66b2e2fcb
Igor Siluianov <olvin.hh@gmail.com>
2019-11-16 23:55:19 +0300

    fix spaces

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, {Component} from 'react';
 import './reset.css';
 import './App.css';
 import {
-  getRecipe,
-  getRecipeList,
-  getStoresNearby,
-  getProductsByStore,
-  getProductDetailsFromStore
+    getRecipe,
+    getRecipeList,
+    getStoresNearby,
+    getProductsByStore,
 } from "./requestFunctions";
 import _ from 'lodash';
 import {PICTURE_LINK} from "./constants";
@@ -19,16 +18,16 @@ import Img from "./Img";
 const AnyReactComponent = () => <div className="marker">&#128315;</div>;
 
 class App extends Component {
-  static defaultProps = MAP_DEFAULT_PROPS;
+    static defaultProps = MAP_DEFAULT_PROPS;
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.handleQueryChange = this.handleQueryChange.bind(this);
-    this.sendSearchRequest = this.sendSearchRequest.bind(this);
-    this.renderRecipes = this.renderRecipes.bind(this);
-    this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
-    this.handleRecipeItemClick = this.handleRecipeItemClick.bind(this);
+        this.handleQueryChange = this.handleQueryChange.bind(this);
+        this.sendSearchRequest = this.sendSearchRequest.bind(this);
+        this.renderRecipes = this.renderRecipes.bind(this);
+        this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+        this.handleRecipeItemClick = this.handleRecipeItemClick.bind(this);
 
         this.state = {
             loading: false,
@@ -43,73 +42,73 @@ class App extends Component {
         };
     }
 
-  componentDidMount() {
-    this.loadStores();
-  }
-
-  handleQueryChange(e) {
-    this.setState({
-      query: e.target.value,
-      searchFieldIsFalid: true
-    })
-  }
-
-  handleSearchSubmit(e) {
-    e.preventDefault();
-    if (this.state.query && this.state.query !== '') {
-      this.sendSearchRequest(this.state.query);
-      if (this.state.recipes.length === 0) {
-        this.setState({
-          recipesFound: false
-        });
-        console.log('no recipes found')
-      } else {
-        this.setState({
-          recipesFound: true
-        });
-      }
-
-    } else {
-      this.setState({
-        searchFieldIsFalid: false
-      })
+    componentDidMount() {
+        this.loadStores();
     }
-  }
 
-  async sendSearchRequest(requestString) {
-    this.setState({loading: true});
-    const response = await getRecipeList(requestString);
-    this.setState({
-      loading: false,
-      recipes: _.keyBy(response.suggestions, item => item.payload),
-    });
-  }
+    handleQueryChange(e) {
+        this.setState({
+            query: e.target.value,
+            searchFieldIsFalid: true
+        })
+    }
 
-  async handleRecipeItemClick(id) {
-    let updatedRecipes = {...this.state.recipes};
+    handleSearchSubmit(e) {
+        e.preventDefault();
+        if (this.state.query && this.state.query !== '') {
+            this.sendSearchRequest(this.state.query);
+            if (this.state.recipes.length === 0) {
+                this.setState({
+                    recipesFound: false
+                });
+                console.log('no recipes found')
+            } else {
+                this.setState({
+                    recipesFound: true
+                });
+            }
+
+        } else {
+            this.setState({
+                searchFieldIsFalid: false
+            })
+        }
+    }
+
+    async sendSearchRequest(requestString) {
+        this.setState({loading: true});
+        const response = await getRecipeList(requestString);
+        this.setState({
+            loading: false,
+            recipes: _.keyBy(response.suggestions, item => item.payload),
+        });
+    }
+
+    async handleRecipeItemClick(id) {
+        let updatedRecipes = {...this.state.recipes};
 
 
-    updatedRecipes = _.mapValues(this.state.recipes, item => {
-      item.expanded = (item.payload === id);
-      return item;
-    });
+        updatedRecipes = _.mapValues(this.state.recipes, item => {
+            item.expanded = (item.payload === id);
+            return item;
+        });
 
-    this.setState({loading: true});
-    const recipes = await getRecipe(id);
-    recipes.forEach(item => {
-      if (!updatedRecipes[item.Id]) {
-        console.log('error');
-      }
-      updatedRecipes[item.Id].data = item;
-      updatedRecipes[item.Id].expanded = true;
-    });
+        this.setState({loading: true});
+        const recipes = await getRecipe(id);
+        recipes.forEach(item => {
+            if (!updatedRecipes[item.Id]) {
+                console.log('error');
+            }
+            updatedRecipes[item.Id].data = item;
+            updatedRecipes[item.Id].expanded = true;
+        });
 
-    this.setState({
-      loading: false,
-      recipes: updatedRecipes,
-      preparationTime: recipes[0].PreparationTime.Description,
-      ingredients: recipes[0].Ingredients[0].SubSectionIngredients.map((item) => item[0].Name),
-      instructions: recipes[0].Instructions,
+        this.setState({
+            loading: false,
+            recipes: updatedRecipes,
+            preparationTime: recipes[0].PreparationTime.Description,
+            ingredients: recipes[0].Ingredients[0].SubSectionIngredients.map((item) => item[0].Name),
+            instructions: recipes[0].Instructions,
 
         });
         // console.log('preparationTime: ', this.state.preparationTime);
@@ -131,7 +130,7 @@ class App extends Component {
                 filteredStores.push(this.state.stores[index]);
             }
         });
-        this.setState({ filteredStores: filteredStores });
+        this.setState({filteredStores: filteredStores});
         console.log(filteredStores);
     }
 
@@ -150,76 +149,76 @@ class App extends Component {
         });
     }
 
-  renderRecipes() {
-    return _.map(this.state.recipes, item => {
-      const id = item.payload;
-      return (
-        <div
-          className={classNames("recipe-item", {
-            "recipe-item--expanded": item.expanded,
-          })}
-          key={id}
-          onClick={() => this.handleRecipeItemClick(id)}
-        >
-          <div className="recipe-item__picture">
-            <Img src={`${PICTURE_LINK}${id}?w=200&h=150&fit=clip`}/>
-          </div>
-          <div className="item-description">
-            <div className="recipe-item__title">{item.suggestion}</div>
-            {item.expanded && <div>
-              <div className="preparation-time">{this.state.preparationTime}</div>
-              <div className="ingredients">{this.state.ingredients}</div>
-              <div className="instructions">{this.state.instructions}</div>
-            </div>}
-          </div>
-        </div>
-      );
-    });
-  }
+    renderRecipes() {
+        return _.map(this.state.recipes, item => {
+            const id = item.payload;
+            return (
+                <div
+                    className={classNames("recipe-item", {
+                        "recipe-item--expanded": item.expanded,
+                    })}
+                    key={id}
+                    onClick={() => this.handleRecipeItemClick(id)}
+                >
+                    <div className="recipe-item__picture">
+                        <Img src={`${PICTURE_LINK}${id}?w=200&h=150&fit=clip`}/>
+                    </div>
+                    <div className="item-description">
+                        <div className="recipe-item__title">{item.suggestion}</div>
+                        {item.expanded && <div>
+                            <div className="preparation-time">{this.state.preparationTime}</div>
+                            <div className="ingredients">{this.state.ingredients}</div>
+                            <div className="instructions">{this.state.instructions}</div>
+                        </div>}
+                    </div>
+                </div>
+            );
+        });
+    }
 
-  render() {
-    return (
-      <>
-        {this.state.loading && <div className="loading-indicator">Loading...</div>}
+    render() {
+        return (
+            <>
+                {this.state.loading && <div className="loading-indicator">Loading...</div>}
 
-        <h1>Save your money. Reduce food waste.</h1>
-        <h2>What do you want to cook?</h2>
-        <form
-          className="request-form"
-          onSubmit={this.handleSearchSubmit}
+                <h1>Save your money. Reduce food waste.</h1>
+                <h2>What do you want to cook?</h2>
+                <form
+                    className="request-form"
+                    onSubmit={this.handleSearchSubmit}
 
-        >
-          <fieldset disabled={this.state.loading}>
-            <input
-              type="text"
-              onChange={this.handleQueryChange}
-              value={this.state.query}
-              className={this.state.searchFieldIsFalid ? 'request-form__input' : 'request-form__input error'}
-              placeholder="Recipe name..."
-              autoFocus
-            />
-            <button className="request-form__button">Search</button>
-          </fieldset>
-        </form>
+                >
+                    <fieldset disabled={this.state.loading}>
+                        <input
+                            type="text"
+                            onChange={this.handleQueryChange}
+                            value={this.state.query}
+                            className={this.state.searchFieldIsFalid ? 'request-form__input' : 'request-form__input error'}
+                            placeholder="Recipe name..."
+                            autoFocus
+                        />
+                        <button className="request-form__button">Search</button>
+                    </fieldset>
+                </form>
 
-        <div className="recipe-wrapper">
-          {this.state.recipesFound ? this.renderRecipes() : 'Sorry, no recieps found for your request'}
-        </div>
-        <div style={{height: '100vh', width: '100%'}}>
-          <GoogleMapReact
-            bootstrapURLKeys={{key: 'AIzaSyAlDha-FyRkP7V7B8E3SyxhtCYqeL_6nPI'}}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          >
-            <AnyReactComponent
-              lat={60.1603071}
-              lng={24.8751406}
-            />
-          </GoogleMapReact>
-        </div>
-      </>
-    );
-  }
+                <div className="recipe-wrapper">
+                    {this.state.recipesFound ? this.renderRecipes() : 'Sorry, no recieps found for your request'}
+                </div>
+                <div style={{height: '100vh', width: '100%'}}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{key: 'AIzaSyAlDha-FyRkP7V7B8E3SyxhtCYqeL_6nPI'}}
+                        defaultCenter={this.props.center}
+                        defaultZoom={this.props.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={60.1603071}
+                            lng={24.8751406}
+                        />
+                    </GoogleMapReact>
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
